f167b9dba5f416144c4a0552e0eda387ddfd0794
duidae <duidae@gmail.com>
2026-08-03 15:03:49 +0800

    feat: add course settings entity

---
+++ backend/src/database/course-withdrawal-db/entities/course-withdrawal-settings.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'course_withdrawal_settings' })
+@Index(['courseId'], { unique: true })
+export class CourseWithdrawalSetting {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ length: 255 })
+  courseId!: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+  })
+  startAt!: Date;
+
+  @Column({
+    type: 'timestamp with time zone',
+  })
+  endAt!: Date;
+
+  @Column({
+    type: 'jsonb',
+    nullable: true,
+  })
+  noticeDelta?: object;
+
+  @Column({
+    default: true,
+  })
+  enabled!: boolean;
+
+  @Column({
+    length: 255,
+  })
+  createdBy!: string;
+
+  @Column({
+    length: 255,
+  })
+  updatedBy!: string;
+
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+  })
+  updatedAt!: Date;
+}
