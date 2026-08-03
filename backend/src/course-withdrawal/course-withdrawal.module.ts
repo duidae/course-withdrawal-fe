5e03c84defedba3a010f474052cce9c6be6c6c01
duidae <duidae@gmail.com>
2026-08-03 09:29:48 +0800

    fix: wire up db and api

--- backend/src/course-withdrawal/course-withdrawal.module.ts
+++ backend/src/course-withdrawal/course-withdrawal.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { CourseWithdrawalDbModule } from '../database/course-withdrawal-db/course-withdrawal-db.module';
 import { CourseWithdrawalController } from './course-withdrawal.controller';
 import { CourseWithdrawalService } from './course-withdrawal.service';
 
 @Module({
+  imports: [CourseWithdrawalDbModule],
   controllers: [CourseWithdrawalController],
   providers: [CourseWithdrawalService],
 })
