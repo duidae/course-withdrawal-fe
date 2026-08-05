e6c3ca92444c0fea5c63457e0f4d855054b7e2a0
duidae <duidae@gmail.com>
2026-08-05 14:17:30 +0800

    fix: rm commited files

--- backend/src/auth/decorators/permission.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { Permission } from '../models/enums/permission.enum';
-
-export const PERMISSIONS_KEY = 'permissions';
-
-export type PermissionRule = [routeParam: string, permission: Permission];
-
-// TODO: no guard reads PERMISSIONS_KEY yet - this only attaches metadata for now.
-export const Permissions = (rule: PermissionRule) => SetMetadata(PERMISSIONS_KEY, rule);
